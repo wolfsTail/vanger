50f146f0944db81971826dc89181f2747fe5181f
DRomanov <lukaviy2la@gmail.com>
2024-05-05 14:42:52 +0300

    slider front complete

--- service/main/views.py
+++ service/main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from slider.models import Slider
+
 
 def get_index(request):
-    return render(request, "main/index.html")
+    context = {}
+    slides = Slider.objects.select_related('owner', 'image').all()
+    context = {'slides': slides}
+    return render(request, "main/index.html", context)
